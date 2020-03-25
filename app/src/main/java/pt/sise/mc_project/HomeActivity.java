72e46d5ed63d9c8a8dd66185038a54c7f59ce606
joaopreis <59958786+joaopreis@users.noreply.github.com>
2020-03-25 22:28:55 +0000

    Commit Teste

--- app/src/main/java/pt/sise/mc_project/HomeActivity.java
+++ app/src/main/java/pt/sise/mc_project/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
+    //change
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
