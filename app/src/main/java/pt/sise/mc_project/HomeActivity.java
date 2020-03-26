da35781b1431f867df9c0be1d1894a3ac3de1419
joaopreis <59958786+joaopreis@users.noreply.github.com>
2020-03-26 11:26:22 +0000

    First Commit 26-03

--- app/src/main/java/pt/sise/mc_project/HomeActivity.java
+++ app/src/main/java/pt/sise/mc_project/HomeActivity.java
@@ -3,6 +3,9 @@ package pt.sise.mc_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
     //change
@@ -11,5 +14,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        final Button profileButton=findViewById(R.id.homePersonalInformationButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","Profile Button Clicked.");
+            }
+        });
+
+        final Button newClaimButton=findViewById(R.id.homeNewClaimButton);
+        newClaimButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","New Claim Button Clicked.");
+            }
+        });
+
+        final Button claimsInformationButton=findViewById(R.id.homeClaimsInformationButton);
+        claimsInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","Claims Information Button Clicked.");
+            }
+        });
+
+        final Button settingsButton=findViewById(R.id.homeSettingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","Settings Button Clicked.");
+            }
+        });
+        }
     }
-}
+
