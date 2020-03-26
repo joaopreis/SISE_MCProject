8f8fd8fb39b1aab90cd9401f2aca2b4cb36989a4
joaopreis <59958786+joaopreis@users.noreply.github.com>
2020-03-26 12:23:31 +0000

    Second Commit 26-03

---
+++ app/src/main/java/pt/sise/mc_project/NewClaimActivity.java
@@ -0,0 +1,33 @@
+package pt.sise.mc_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class NewClaimActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_claim);
+
+        final Button backButton=findViewById(R.id.newClaimBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","Back Button Clicked.");
+            }
+        });
+
+        final Button submitButton=findViewById(R.id.newClaimSubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SISE","Submit Button Clicked.");
+            }
+        });
+    }
+}
